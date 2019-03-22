b97afe9a88b9d73e5d779c27f49100bc29d641c1
Dmitry Strukovsky <strukovsky1@gmail.com>
2019-03-22 13:56:27 +0300

    add insert async tasks

--- app/src/main/java/strukovsky/app/makeday/room/Repository.kt
+++ app/src/main/java/strukovsky/app/makeday/room/Repository.kt
@@ -8,8 +8,7 @@ import android.os.AsyncTask
  * Created by dmitry on 14.03.2019.
  */
 
-class Repository(context: Context)
-{
+class Repository(context: Context) {
     val database: Database = Database.getInstance(context)
     val timetableDao = database.timetableDao
     val actionDao = database.actionDao
@@ -20,26 +19,39 @@ class Repository(context: Context)
     fun selectActions(timetable_id: Int, time: String) = AsyncSelectActions(timetable_id = timetable_id, time = time, dao = actionDao).execute().get()!!
 
 
-    class AsyncGetAllTimetables(val dao: TimetableDao): AsyncTask<Unit, Unit, LiveData<List<Timetable>>>()
-    {
+    class AsyncGetAllTimetables(val dao: TimetableDao) : AsyncTask<Unit, Unit, LiveData<List<Timetable>>>() {
         override fun doInBackground(vararg p0: Unit): LiveData<List<Timetable>> {
             return dao.getAllTimetables()
         }
     }
 
-    class AsyncGetAllActions(val dao: ActionDao): AsyncTask<Unit, Unit, LiveData<List<Action>>>(){
+    class AsyncGetAllActions(val dao: ActionDao) : AsyncTask<Unit, Unit, LiveData<List<Action>>>() {
         override fun doInBackground(vararg p0: Unit): LiveData<List<Action>> {
             return dao.getAllActions()
         }
     }
 
-    class AsyncSelectActions(val dao: ActionDao, val timetable_id: Int,val time: String): AsyncTask<Unit, Unit, LiveData<List<Action>>>()
-    {
+    class AsyncSelectActions(val dao: ActionDao, val timetable_id: Int, val time: String) : AsyncTask<Unit, Unit, LiveData<List<Action>>>() {
         override fun doInBackground(vararg p0: Unit): LiveData<List<Action>> {
             return dao.selectActions(timetable_id, time)
         }
 
     }
 
-     
+    class AsyncInsertTimetable(val dao: TimetableDao, val timetable: Timetable) : AsyncTask<Unit, Unit, Unit>() {
+        override fun doInBackground(vararg p0: Unit?) {
+            dao.insert(timetable)
+        }
+
+    }
+
+    class AsyncInsertAction(val dao: ActionDao, val action: Action): AsyncTask<Unit, Unit, Unit>()
+    {
+        override fun doInBackground(vararg p0: Unit?) {
+            dao.insert(action)
+        }
+
+    }
+
+
 }
